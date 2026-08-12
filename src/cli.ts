46d3cf75406c60c614769d7ee21ac3dfc0ed4cf6
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2026-08-13 01:38:29 +0200

    Further improvements on stdin/process/kb control

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,8 @@ import { emitKeypressEvents, Key } from "node:readline";
 import { createInterface } from "node:readline/promises";
 import { spawn, ChildProcess } from "node:child_process";
 
+type KeyHandler = (str: string, key: Key) => void;
+
 enum CLIState {
   STREAMING = "STREAMING",
   PROMPT = "PROMPT",
@@ -11,7 +13,7 @@ enum CLIState {
 
 export class CLI {
   private state: CLIState = CLIState.STREAMING;
-  private keypressHandler: ((str: string, key: Key) => void) | null = null;
+  private keypressHandler: KeyHandler | null = null;
   private acp_process: ChildProcess | null = null;
   private acp_chunks: Buffer[] = [];
 
@@ -25,77 +27,11 @@ export class CLI {
     });
     this.acp_process.on("error", (err) => {
       stderr.write(`Failed to execute ACP command: ${err.message}\n`);
-      exit(1);
+      this.cleanupAndExit(-1);
     });
 
     emitKeypressEvents(stdin);
-    this.enableRawMode();
-  }
-
-  private enableRawMode(): void {
-    stdin.setRawMode(true);
-    stdin.resume();
-    if (!this.keypressHandler) {
-      this.keypressHandler = (_str, key) => {
-        if (key.name === "escape" || (key.ctrl && key.name === "c")) {
-          this.state = CLIState.INTERRUPTED;
-          this.disableRawMode();
-          this.promptHandler();
-        }
-      };
-      stdin.on("keypress", this.keypressHandler);
-    }
-  }
-
-  private disableRawMode(): void {
-    if (this.keypressHandler) {
-      stdin.removeListener("keypress", this.keypressHandler);
-      this.keypressHandler = null;
-    }
-    stdin.setRawMode(false);
-  }
-
-  private promptHandler() {
-    (async () => {
-      let promptSymbol: string = "> ";
-      if (this.state === CLIState.INTERRUPTED) {
-        if (this.acp_chunks.length > 1) {
-          promptSymbol = "[echo] > ";
-        } else {
-          promptSymbol = "[int] > ";
-        }
-      }
-
-      const rl = createInterface({ input: stdin, output: stdout });
-
-      rl.on("SIGINT", () => {
-        // TODO: Handle interruption
-      });
-
-      let on_prompt: boolean = true;
-      while (on_prompt) {
-        const command = await rl.question(promptSymbol);
-        const full_command = command.trim();
-
-        switch (full_command) {
-          case "/quit":
-          case "/exit":
-            // TODO: Handle ACP state when quit is requested
-            this.flushChunks();
-            rl.close();
-            exit(0);
-          case "/continue":
-            this.state = CLIState.STREAMING;
-            this.flushChunks();
-            rl.close();
-            this.enableRawMode();
-            on_prompt = false;
-            break;
-          default:
-            break;
-        }
-      }
-    })();
+    this.handleStreaming();
   }
 
   private handleACPChunk(chunk: Buffer) {
@@ -106,13 +42,109 @@ export class CLI {
     }
   }
 
-  // TODO: Handle ACP close
-  private handleACPClose(_code: number) {}
+  private handleACPClose(code: number) {
+    this.cleanupAndExit(code);
+  }
+
+  private cleanupAndExit(code: number) {
+    this.flushChunks();
+    // TODO: Handle ACP state when quit is requested
+    // if this.acp_process ...
+    stdin.setRawMode(false);
+    exit(code);
+  }
 
   private flushChunks() {
     if (this.acp_chunks.length > 0) {
       stdout.write(Buffer.concat(this.acp_chunks).toString());
       this.acp_chunks = [];
+    }
+  }
+
+  private handleStreaming(): void {
+    this.state = CLIState.STREAMING;
+    stdin.setRawMode(true);
+    stdin.resume();
+    if (!this.keypressHandler) {
+      this.keypressHandler = (_str, key) => {
+        // TODO: This should stop the agent mid-streamning
+        // if (key && (key.name === "escape" || (key.ctrl && key.name === "c"))) {
+        if (key && key.name === "return") {
+          this.state = CLIState.INTERRUPTED;
+          if (this.keypressHandler) {
+            stdin.removeListener("keypress", this.keypressHandler);
+            this.keypressHandler = null;
+          }
+          this.handlePrompt();
+        }
+      };
+      stdin.on("keypress", this.keypressHandler);
+    }
+  }
+
+  private handlePrompt() {
+    (async () => {
+      let on_prompt: boolean = true;
+      const rl = createInterface({ input: stdin, output: stdout });
+      try {
+        while (on_prompt) {
+          const ac = new AbortController();
+          if (!this.keypressHandler) {
+            this.keypressHandler = (_str, key) => {
+              if (key && key.name === "escape") {
+                ac.abort();
+              }
+            };
+          }
+          stdin.on("keypress", this.keypressHandler);
+
+          // TODO: Handle interruption
+          rl.on("SIGINT", () => {
+            this.cleanupAndExit(0);
+          });
+
+          const command = await rl.question(this.makePrompt(), {
+            signal: ac.signal,
+          });
+
+          switch (command.trim()) {
+            case "/quit":
+            case "/exit":
+              this.cleanupAndExit(0);
+            case "/continue":
+              this.flushChunks();
+              on_prompt = false;
+              break;
+            default:
+              break;
+          }
+        }
+      } catch (err: any) {
+        if (err.name === "AbortError") {
+          this.flushChunks();
+          on_prompt = false;
+        }
+      } finally {
+        if (this.keypressHandler) {
+          stdin.removeListener("keypress", this.keypressHandler);
+          this.keypressHandler = null;
+        }
+        rl.close();
+      }
+
+      this.handleStreaming();
+    })();
+  }
+
+  private makePrompt(): string {
+    if (this.state === CLIState.INTERRUPTED) {
+      if (this.acp_chunks.length > 1) {
+        return "[echo] > ";
+      } else {
+        return "[int] > ";
+      }
+    } else {
+      return "> ";
     }
   }
 }
